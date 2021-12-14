5b67364de471d58c89c2199c925dbf1158946848
albin-joseph <albinjoseph007@gmail.com>
2021-12-14 13:37:27 +0530

    add dispatch global ques examples

--- MyPlayground1.playground/Contents.swift
+++ MyPlayground1.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import Fou
 
 //Difference between Variable, Constants and static
 
@@ -23,3 +24,49 @@ print(p.name)
 print(Person.specious)
 
 //If we declare a variable with Static keyword, only one copy of variable available for all objects and we can acces the varible without an instance
+
+class sammpleClass{
+    let sampleVariable = "Sample String"
+}
+
+if(2_000 == 2000){
+    
+}
+
+DispatchQueue.global().async {
+    <#code#>
+}
+
+DispatchQueue.global(qos: .background).async {
+    <#code#>
+}
+
+DispatchQueue(label: "com.dispatch.serial", qos: .background, attributes: .concurrent).async {
+    <#code#>
+}
+
+//Dospactch grops groups dispatches together
+
+let group = DispatchGroup()
+
+let items:[TimeInterval] = [
+    1,
+    3,
+    7,
+    5,
+    3,
+    2,
+    6
+]
+
+for el in items{
+    group.enter()
+    DispatchQueue.global().asyncAfter(deadline: now()+el) {
+        print("enter")
+        group.leave()
+    }
+}
+
+group.notify(queue: .main) {
+    print("notify")
+}
--- MyPlayground3.playground/Contents.swift
+++ MyPlayground3.playground/Contents.swift
@@ -2,3 +2,19 @@ import Cocoa
 
 var greeting = "Hello, playground"
 print(greeting)
+
+//Example for GCD main que
+
+DispatchQueue.main.async {
+    //We can do UI update here
+}
+
+DispatchQueue.global(qos: .background).async {
+    //call api or some work in back ground
+    DispatchQueue.main.async {
+        //We can do UI update here
+    }
+}
+
+
+//Example for Dispatch
