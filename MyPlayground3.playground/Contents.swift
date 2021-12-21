53eeee9c5c7ef9e1dc1774c4a0eb72f928ca304b
albin-joseph <albinjoseph007@gmail.com>
2021-12-21 20:12:45 +0530

    ass subscript example

--- MyPlayground3.playground/Contents.swift
+++ MyPlayground3.playground/Contents.swift
@@ -18,3 +18,33 @@ DispatchQueue.global(qos: .background).async {
 
 
 //Example for Dispatch
+
+
+let codes = ["abc","def","ghi","OPQ"]
+let newCodes = codes.flatMap { $0.uppercased() }
+print(newCodes)
+
+
+let codes1 = [1, 2, 3, 4]
+let newCodes1 = codes1.flatMap { $0 }
+print(newCodes1)
+
+//Implementation of Subscript
+//Subscripts are used to access information from a collection, sequence and a list in Classes, Structures and Enumerations without using a method.
+class SchoolClass{
+    var students = ["Albin", "Anu"]
+    
+    subscript(_ index:Int) -> String{
+        get{
+          return students[index]
+        }
+        set(newValue){
+            self.students[index] = newValue
+        }
+    }
+}
+
+var tenthStd = SchoolClass()
+print(tenthStd[1])
+tenthStd[1]="Emmanuel"
+print(tenthStd.students)
